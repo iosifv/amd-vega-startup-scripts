99ce5ca6af14f5c08bb88c3e7e249f2846fa7db2
Iosif Vigh <dev@iosifv.com>
2018-06-28 13:20:03 +0100

    [master] - rename files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # rx-vega-startup
 AMD's RX Vega cards need to be rebooted every time in order to mine at full speed. This helps you set up this process to be automatic.
+
+
+## 1. Copy/Clone
+
+  This usage example uses C:/vega-startup/
+  so clone this repo in that path.
+
+## 2. 
--- start-ps-elevated.cmd
+++ start-ps-elevated.cmd
@@ -1,2 +1,2 @@
-PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "C:\windowsScripts\StartupLog.txt" 2>&1
-PowerShell C:\windowsScripts\re-boot-vegas.ps1 >> "C:\windowsScripts\StartupLog.txt" 2>&1
+PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "C:\vega-startup\StartupLog.txt" 2>&1
+PowerShell C:\vega-startup\re-boot-vegas.ps1 >> "C:\vega-startup\StartupLog.txt" 2>&1
